e80e1cdfcaa5355356d28edbb2935078233e6ac1
André <andrelourencoo1750@gmail.com>
2023-12-19 21:49:54 +0000

    Finalizada funcionalidade de escolha de género com ativação de campos escondidos

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const genderAddons = document.getElementById("gender-addon");
+
+const genderRadios = document.querySelectorAll(".gender-selection fieldset input");
+
 const selects = document.querySelectorAll("form select");
+
 
 for(let i = 1; i <= 31; i++) {
     const option = document.createElement("option");
@@ -12,3 +17,12 @@ for(let i = 2023; i >= 1905; i--) {
     selects[2].appendChild(option).textContent = i;
 }
 
+for(let radio of genderRadios) {
+    radio.addEventListener("change", function() {
+        genderAddons.classList.add("hide");
+
+        if(radio.value === "P") {
+            genderAddons.classList.remove("hide");
+        }
+    });
+}
